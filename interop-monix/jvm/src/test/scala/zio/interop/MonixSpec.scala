38d330860d33b71365b927a55a1e63383a0ed20e
Scala Steward <me@scala-steward.org>
2020-03-04 07:02:18 +0100

    Update zio, zio-test, zio-test-sbt to 1.0.0-RC18-1

--- interop-monix/jvm/src/test/scala/zio/interop/MonixSpec.scala
+++ interop-monix/jvm/src/test/scala/zio/interop/MonixSpec.scala
@@ -21,7 +21,7 @@ object MonixSpec
             val task                                          = eval.Task.raiseError[Int](error)
             val io                                            = IO.fromTask(task).unit
             val assertion: Assertion[Either[Throwable, Unit]] = equalTo(Left(error))
-            assertM(io.either, assertion)
+            assertM(io.either)(assertion)
           },
           testM("return an `IO` that produces the value from `Task`.") {
             implicit val scheduler: Scheduler                = Scheduler(runtime.platform.executor.asEC)
@@ -29,25 +29,25 @@ object MonixSpec
             val task                                         = eval.Task(value)
             val io                                           = IO.fromTask(task)(scheduler)
             val assertion: Assertion[Either[Throwable, Int]] = equalTo(Right(value))
-            assertM(io.either, assertion)
+            assertM(io.either)(assertion)
           }
         ),
         suite("IO.toTask")(
           testM("produce a successful `IO` of `Task`.") {
             val task = IO.fail(new Exception).toTask
-            assertM(task.map(_.isInstanceOf[eval.Task[Unit]]), isTrue)
+            assertM(task.map(_.isInstanceOf[eval.Task[Unit]]))(isTrue)
           },
           testM("returns a `Task` that fails if `IO` fails.") {
             val error                                    = new Exception
             val task                                     = IO.fail(error).toTask
             val assertion: Assertion[eval.Task[Nothing]] = equalTo(eval.Task.raiseError(error))
-            assertM(task, assertion)
+            assertM(task)(assertion)
           },
           testM("returns a `Task` that produces the value from `IO`.") {
             implicit val scheduler: Scheduler = Scheduler(runtime.platform.executor.asEC)
             val value                         = 10
             val task                          = IO.succeed(value).toTask.map(_.runSyncUnsafe())
-            assertM(task, equalTo(10))
+            assertM(task)(equalTo(10))
           }
         ),
         suite("IO.fromCoeval")(
@@ -56,32 +56,32 @@ object MonixSpec
             val coeval                                        = eval.Coeval.raiseError[Int](error)
             val io                                            = IO.fromCoeval(coeval).unit
             val assertion: Assertion[Either[Throwable, Unit]] = equalTo(Left(error))
-            assertM(io.either, assertion)
+            assertM(io.either)(assertion)
           },
           testM("return an `IO` that produces the value from `Coeval`.") {
             val value                                        = 10
             val coeval                                       = eval.Coeval(value)
             val io                                           = IO.fromCoeval(coeval)
             val assertion: Assertion[Either[Throwable, Int]] = equalTo(Right(value))
-            assertM(io.either, assertion)
+            assertM(io.either)(assertion)
           }
         ),
         suite("IO.toCoeval")(
           testM("produce a successful `IO` of `Coeval`") {
             val task = IO.fail(new Exception).toCoeval
-            assertM(task.map(_.isInstanceOf[eval.Coeval[Unit]]), isTrue)
+            assertM(task.map(_.isInstanceOf[eval.Coeval[Unit]]))(isTrue)
           },
           testM("returns a `Coeval` that fails if `IO` fails.") {
             val error                                      = new Exception
             val coeval                                     = IO.fail(error).toCoeval
             val assertion: Assertion[eval.Coeval[Nothing]] = equalTo(eval.Coeval.raiseError(error))
-            assertM(coeval, assertion)
+            assertM(coeval)(assertion)
           },
           testM("returns a `Coeval` that produces the value from `IO`.") {
             val value                          = 10
             val coeval                         = IO.succeed(value).toCoeval.map(_.runTry)
             val assertion: Assertion[Try[Int]] = equalTo(Success(10))
-            assertM(coeval, assertion)
+            assertM(coeval)(assertion)
           }
         )
       )
