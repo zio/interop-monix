475f72c956e4b25c57f517330353d5d18c956575
Lachlan O'Dea <lodea@zendesk.com>
2023-01-25 14:57:42 +1100

    Scala 3.2 is now required.

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -25,7 +25,7 @@ object BuildHelper {
 
   val Scala212: String = versions("2.12")
   val Scala213: String = versions("2.13")
-  val Scala3: String   = versions("3.1")
+  val Scala3: String   = versions("3.2")
 
   private val stdOptions = Seq(
     "-deprecation",
